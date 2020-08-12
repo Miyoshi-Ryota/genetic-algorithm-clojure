5d36c2dee36eccd99d2c0428fec067c791fe8a7a
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-08-12 09:40:44 +0900

    disable gpg sign

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject genetic-algorithm "0.1.1"
-  :description "Clojure's implementation of genetic algorithm
+  :description "Clojure's implementation of genetic algorithm.
   see also https://github.com/Miyoshi-Ryota/genetic-algorithm-clojure"
   :url "https://github.com/Miyoshi-Ryota/genetic-algorithm-clojure"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/mit-license.php"}
+  :repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns genetic-algorithm.core})
